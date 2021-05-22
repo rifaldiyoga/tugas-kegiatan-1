c0a46f0fc448f270fa20886d76a2437fb80f5a09
aldi <rifaldi.yoga@bee.id>
2021-05-22 15:59:22 +0700

    initial

---
+++ resources/views/contactus291.blade.php
@@ -0,0 +1,12 @@
+@extends('app')
+
+@section('content')
+<div>
+    <h1>Biodata</h1>
+    <img src="assets/foto.jpg" alt="">
+    <p>Nama : Rifaldi Yoga A</p>
+    <p>Email : agoy587@gmail.com</p>
+    <p>Instagram : rifaldi.yoga</p>
+    
+</div>
+@endsection
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('artikel') }}">Artikel</a>
+    <a href="{{ route('contactus') }}">Contact Us</a>
+</div>
